8b1419959dd9725db1a74cea4d0db294fe308d33
Pearla Mutombo <pmutombo1986@gmail.com>
2026-04-28 13:30:17 -0400

    corrected script.js code

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-const color = [ 'red', 'orange', 'yellow', 'blue', 'green'];
+const colors = ['red', 'orange', 'yellow', 'blue', 'green']; /* declare an array of five strings */
 
-console.log(color.two);
+console.log(colors[1]); /* display "orange" without a loop */
+
+const userColor = colors[1]; /* store "orange" into a variable */
+
+console.log(userColor); /* display the value stored in the item variable */
